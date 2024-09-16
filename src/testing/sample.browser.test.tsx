3a6ecd5f1aa79351959bccf35a5da8c431b3849a
waonpad <waonpad@gmail.com>
2024-09-17 04:50:16 +0900

    add test

--- src/testing/sample.browser.test.tsx
+++ src/testing/sample.browser.test.tsx
@@ -148,4 +148,44 @@ describe("検証", () => {
       `"iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAIAAAD/gAIDAAAAAXNSR0IArs4c6QAAAW9JREFUeJzt3cFNw0AUQMENUAAlUIo7I+kspdAJHIIixNpSnhCKwDOnKPLBefq78cl7GMfBjR7ufQN/iViBWIFYwdP612/LOL9+ftib5TTGGMvKH99arPPxs9Q+XX/71GtahjsvdXV+nVeVPWvbNDTzZBmrTSZrm2X4E2IFYgViBRtP8FuOh1+6j7s5vt9+rckKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECurbJMO7F/8fkxWIFYgViBVMsV7O97mRv8Bkbbsck/LFFGu6gqvH8e3sgee3MQ57PG7nm+U0HyWz9lB6vWiHh39ctuzltLp3HxzZdzsbfCBWIFYgVvABDzgh/1Gqf4kAAAAASUVORK5CYII="`
     );
   });
+
+  test("キャンバスを使用、角丸の矩形を描画、1秒待ってみる", async () => {
+    const width = 100;
+    const height = 100;
+
+    const Comp = () => {
+      return (
+        <div>
+          <canvas width={width} height={height} data-testid="canvas1" />
+        </div>
+      );
+    };
+
+    const { container } = render(<Comp />);
+
+    const canvas1 = container.querySelector(
+      "[data-testid=canvas1]"
+    ) as HTMLCanvasElement;
+
+    const canvas1Context = canvas1.getContext("2d") as CanvasRenderingContext2D;
+
+    canvas1Context.fillStyle = "green";
+    canvas1Context.fillRect(0, 0, width, height);
+
+    canvas1Context.lineJoin = "round";
+    canvas1Context.lineWidth = 10;
+    canvas1Context.strokeStyle = "blue";
+    canvas1Context.strokeRect(10, 10, 80, 80);
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    const { base64: base64_1 } = await page.screenshot({
+      element: canvas1,
+      base64: true,
+    });
+
+    expect(base64_1).toMatchInlineSnapshot(
+      `"iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAIAAAD/gAIDAAAAAXNSR0IArs4c6QAAAW9JREFUeJzt3cFNw0AUQMENUAAlUIo7I+kspdAJHIIixNpSnhCKwDOnKPLBefq78cl7GMfBjR7ufQN/iViBWIFYwdP612/LOL9+ftib5TTGGMvKH99arPPxs9Q+XX/71GtahjsvdXV+nVeVPWvbNDTzZBmrTSZrm2X4E2IFYgViBRtP8FuOh1+6j7s5vt9+rckKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECsQKxArECurbJMO7F/8fkxWIFYgViBVMsV7O97mRv8Bkbbsck/LFFGu6gqvH8e3sgee3MQ57PG7nm+U0HyWz9lB6vWiHh39ctuzltLp3HxzZdzsbfCBWIFYgVvABDzgh/1Gqf4kAAAAASUVORK5CYII="`
+    );
+  });
 });
